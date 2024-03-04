9b8efd27bbcb81a4c64c565d4e0f52f2e0eff9bd
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-03-04 16:17:56 -0300

    feat(app): init structure

---
+++ src/app/@admin/pages/ordes/ordes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ordes',
+  standalone: true,
+  imports: [],
+  templateUrl: './ordes.component.html',
+  styleUrl: './ordes.component.scss'
+})
+export class OrdesComponent {
+
+}
