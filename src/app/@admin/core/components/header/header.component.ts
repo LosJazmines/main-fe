902b336e605b28899b99a3c617d50633dfd44cc8
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-03-06 21:22:09 -0300

    fixer(app) search components - ok stryle

--- src/app/@admin/core/components/header/header.component.ts
+++ src/app/@admin/core/components/header/header.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../../../@shared/material/material.module';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SearchModernoReactiveModule } from '../search-moderno-reactive/search-moderno-reactive.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SearchModernoReactiveModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isPanelToggle = true;
+
+  showResetButton = false;
+
+  searchForm!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.searchForm = this._formBuilder.group({
+      search: [''],
+    });
+  }
+
+  panelToggle() {
+    return (this.isPanelToggle = !this.isPanelToggle);
+  }
+
+  search() {
+    console.log('Seach click');
+  }
+
+  onSubmit() {}
+
+  onClearSearch() {}
+
+  handleSearch(event: any) {}
+}
